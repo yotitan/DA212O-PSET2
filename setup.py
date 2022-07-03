0bcf3fdbd7546d8c7c47fdfdf8b982fe9209c447
yotitan <yotitan@gmail.com>
2022-07-03 12:51:27 +0530

    Cookiecutter project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='DA212O-PSET2',
+    author='Parth Dhir',
+    license='',
+)
